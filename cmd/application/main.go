e1cd55ea8edca18c078250e06ad59b9febe39500
ASSOSSOU <zayde@MacBook-Pro-de-ASSOSSOU.local>
2026-01-12 11:24:00 +0100

    afficher les artistes

--- cmd/application/main.go
+++ cmd/application/main.go
@@ -1,15 +1,54 @@
 package main
 
 import (
+	"groupie-tracker/api"
+	interfacegraphique "groupie-tracker/interface"
+	"groupie-tracker/modele"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+const baseURL = "https://groupietrackers.herokuapp.com"
+
+type runner interface {
+	RunOnMain(func())
+}
+
+func executerSurThreadPrincipal(f func()) {
+	// Certains drivers Fyne ont RunOnMain mais l'interface fyne.Driver ne l'expose pas.
+	// On fait donc une assertion de type.
+	if r, ok := fyne.CurrentApp().Driver().(runner); ok {
+		r.RunOnMain(f)
+		return
+	}
+	// Fallback : on exécute directement (souvent ça marche, mais c'est moins sûr)
+	f()
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Groupie Tracker")
-	w.SetContent(widget.NewLabel("Fyne fonctionne ✅"))
-	w.Resize(fyne.NewSize(420, 240))
+	w.Resize(fyne.NewSize(900, 600))
+
+	loading := widget.NewLabel("Chargement des artistes…")
+	w.SetContent(container.NewCenter(loading))
+	w.Show()
+
+	go func() {
+		var artistes []modele.Artiste
+		err := api.RecupererJSON(baseURL+"/api/artists", &artistes)
+
+		executerSurThreadPrincipal(func() {
+			if err != nil {
+				w.SetContent(container.NewCenter(widget.NewLabel("Erreur : " + err.Error())))
+				return
+			}
+			w.SetContent(interfacegraphique.VueAccueil(artistes))
+		})
+	}()
+
 	w.ShowAndRun()
 }
---
+++ interface/accueil.go
@@ -0,0 +1,22 @@
+package interfacegraphique
+
+import (
+	"groupie-tracker/modele"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func VueAccueil(artistes []modele.Artiste) fyne.CanvasObject {
+	liste := widget.NewList(
+		func() int { return len(artistes) },
+		func() fyne.CanvasObject { return widget.NewLabel("...") },
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(artistes[i].Nom)
+		},
+	)
+
+	titre := widget.NewLabelWithStyle("Artistes", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+	return container.NewBorder(titre, nil, nil, nil, liste)
+}
