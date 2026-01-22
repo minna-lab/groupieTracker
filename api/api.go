e26d499fac152cb506f8a72fec553b44d9d3d3bc
RameixR <raphael.rameix@ynov.com>
2026-01-22 10:55:22 +0100

    ajout des commentaire dans le fichier api

--- api/api.go
+++ api/api.go
@@ -49,3 +49,29 @@ func RecupererRelation(id int) (modele.Relation, error) {
 	)
 	return relation, err
 }
+
+// RecupererLocations récupère les lieux de concerts disponibles depuis l'API
+// Retourne une structure Lieux contenant tous les lieux de concerts
+func RecupererLocations() ([]modele.Lieux, error) {
+	var locations struct {
+		Index []modele.Lieux `json:"index"`
+	}
+	err := RecupererJSON(
+		"https://groupietrackers.herokuapp.com/api/locations",
+		&locations,
+	)
+	return locations.Index, err
+}
+
+// RecupererDates récupère les dates de concerts disponibles depuis l'API
+// Retourne un tableau de structures Dates contenant toutes les dates de concerts
+func RecupererDates() ([]modele.Dates, error) {
+	var dates struct {
+		Index []modele.Dates `json:"index"`
+	}
+	err := RecupererJSON(
+		"https://groupietrackers.herokuapp.com/api/dates",
+		&dates,
+	)
+	return dates.Index, err
+}
